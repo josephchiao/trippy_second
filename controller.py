83b53a34915189260831741f10df40a12702b7d0
josephchiao <chiaofeng.0118@gmail.com>
2026-08-10 11:27:47 -0700

    changed network size

--- controller.py
+++ controller.py
@@ -110,7 +110,7 @@ class SP_Controller:
                     self.stable_counter = 0      
         
         elif mode == 'ML':
-            NN = nn.NeuralNetwork((4, 64, 64, 2), [nn.ReLU, nn.ReLU, [nn.linear, nn.sigmoid]], 'nn_library')
+            NN = nn.NeuralNetwork((4, 16, 16, 2), [nn.ReLU, nn.ReLU, [nn.linear, nn.sigmoid]], 'nn_library')
             NN.theta_recover(0)
             self.motor_force = (NN.feedforward(RL_trainer.normalize(self, state = self.pendulum.state))[-1][0][1] - 0.5) * 200
             state_string = 'ML control'
