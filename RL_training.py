5230c360a837d656f26fadb46d2c4735c6c9137b
josephchiao <chiaofeng.0118@gmail.com>
2026-08-11 12:00:36 -0700

    setup changes

--- RL_training.py
+++ RL_training.py
@@ -6,7 +6,7 @@ from scipy.integrate import solve_ivp
 import random
 import physics
 
-init_log_std = 0.3  # Initial exploration noise level (log scale)
+init_log_std = 0  # Initial exploration noise level (log scale)
 
 class RL_trainer:
 
@@ -20,8 +20,8 @@ class RL_trainer:
         self.NN_V = nn.NeuralNetwork((4, 16, 16, 1), [nn.ReLU, nn.ReLU, nn.linear], 'V_nn_library')
         self.NN_mu = nn.NeuralNetwork((4, 16, 16, 1), [nn.ReLU, nn.ReLU, nn.sigmoid], 'mu_nn_library')
 
-        # self.NN_V.theta_generate()
-        # self.NN_mu.theta_generate()
+        self.NN_V.theta_generate()
+        self.NN_mu.theta_generate()
 
         self.NN_V.theta_recover()
         self.NN_mu.theta_recover()
@@ -33,8 +33,8 @@ class RL_trainer:
 
         """Max reward should be 1. Reward is based on how upright the pendulum is and how close the cart is to the center."""
 
-        location_cf = 0.4
-        angle_cf = 0.6
+        location_cf = 0
+        angle_cf = 1
         time_reward = 0
     
         reward = (time_reward - angle_cf * math.cos(state[1]) + location_cf/(4*abs(state[0])+1))
@@ -100,7 +100,7 @@ class RL_trainer:
 
         for episode in range(1000000):
 
-            learning_rate = 0.00005
+            learning_rate = 0.00004
             V_lrn = 10  # Critic learning rate multiplier
             random_angle = np.pi/120
             random_location = 0
